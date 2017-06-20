d42b543de8cf31edf93e75bad0f54c5561448d45
juanqenk <juanqenk@gmail.com>
2017-06-20 20:21:15 +0200

    Añado como volver al presente en la rama

---
+++ backtothefuture.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[3]:
+
+import time
+
+
+# In[4]:
+
+print('Estimado viajero en el tiempo, gracias por confiar en TimeTraveler para volver al presente.')
+print('Esperamos que no haya evitado que sus padres se conozcan. ')
+
+
+# In[5]:
+
+print('Abrochense los cinturones. Usted va a viajar al día', time.asctime())
+print('Done!')
+print('Bienvenido al presente, le echabamos de menos. ')
+
+
+# In[ ]:
+
+
+
